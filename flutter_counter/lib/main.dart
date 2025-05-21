af760b34a4afb602bb10c87d9b4d398436f8d2ba
YongJunCha <60722292+kuruma-42@users.noreply.github.com>
2025-05-21 16:14:10 +0900

    feat: main 화면 수정

--- flutter_counter/lib/main.dart
+++ flutter_counter/lib/main.dart
@@ -1,6 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_counter/counter/view/counter_page.dart';
 
-class CounterApp extends MaterialApp {
-  const CounterApp({super.key}) : super(home: const CounterPage());
+void main() {
+  runApp(const CounterApp());
+}
+
+class CounterApp extends StatelessWidget {
+  const CounterApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: CounterPage(),
+    );
+  }
 }
