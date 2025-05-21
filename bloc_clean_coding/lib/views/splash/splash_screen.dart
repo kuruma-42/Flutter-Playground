2270975c1fe2f10c3a3d32194035a0df4b771749
YongJunCha <60722292+kuruma-42@users.noreply.github.com>
2025-05-21 15:56:16 +0900

    feat: splash 화면 구현

--- bloc_clean_coding/lib/views/splash/splash_screen.dart
+++ bloc_clean_coding/lib/views/splash/splash_screen.dart
@@ -1,3 +1,4 @@
+import 'package:bloc_clean_coding/config/components/round_button.dart';
 import 'package:bloc_clean_coding/config/routes/route_name.dart';
 import 'package:flutter/material.dart';
 
@@ -12,11 +13,25 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: TextButton(
-        onPressed: () {
-          Navigator.pushNamed(context, RoutesName.homeScreen);
-        },
-        child: Text('Home'),
+      body: SafeArea(
+        child: Center(
+          child: Column(
+            children: [
+              RoundButton(
+                title: 'Login',
+                onPress: () {
+                  Navigator.pushNamed(context, RoutesName.loginScreen);
+                },
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.pushNamed(context, RoutesName.homeScreen);
+                },
+                child: const Text('Home'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
