a34c7c83d89137a1b1db4bccd266730809c7ba96
YongJunCha <60722292+kuruma-42@users.noreply.github.com>
2025-05-30 16:36:17 +0900

    feat: login_bloc 기능 수정

--- flutter_login/lib/login/bloc/login_bloc.dart
+++ flutter_login/lib/login/bloc/login_bloc.dart
@@ -1,13 +1,66 @@
+import 'package:authentication_repository/authentication_repository.dart';
 import 'package:bloc/bloc.dart';
 import 'package:equatable/equatable.dart';
+import 'package:flutter_login/login/models/password.dart';
+import 'package:flutter_login/login/models/username.dart';
+import 'package:formz/formz.dart';
 
 part 'login_event.dart';
 part 'login_state.dart';
 
 class LoginBloc extends Bloc<LoginEvent, LoginState> {
-  LoginBloc() : super(LoginInitial()) {
-    on<LoginEvent>((event, emit) {
-      // TODO: implement event handler
-    });
+  LoginBloc({required AuthenticationRepository authenticationRepository})
+    : _authenticationRepository = authenticationRepository,
+      super(LoginState()) {
+    on<LoginUserNameChanged>(_onUsernameChanged);
+    on<LoginPasswordChanged>(_onPasswordChanged);
+    on<LoginSubmitted>(_onSubmitted);
+  }
+
+  final AuthenticationRepository _authenticationRepository;
+
+  void _onUsernameChanged(
+    LoginUserNameChanged event,
+    Emitter<LoginState> emit,
+  ) {
+    final username = Username.dirty(event.username);
+    emit(
+      state.copyWith(
+        username: username,
+        isValid: Formz.validate([username, state.password]),
+      ),
+    );
+  }
+
+  void _onPasswordChanged(
+    LoginPasswordChanged event,
+    Emitter<LoginState> emit,
+  ) {
+    final password = Password.dirty(event.password);
+    emit(
+      state.copyWith(
+        password: password,
+        isValid: Formz.validate([state.username, password]),
+      ),
+    );
+  }
+
+  Future<void> _onSubmitted(
+    LoginSubmitted event,
+    Emitter<LoginState> emit,
+  ) async {
+    if (!state.isValid) return;
+
+    emit(state.copyWith(status: FormzSubmissionStatus.inProgress));
+
+    try {
+      await _authenticationRepository.logIn(
+        username: state.username.value,
+        password: state.password.value,
+      );
+      emit(state.copyWith(status: FormzSubmissionStatus.success));
+    } catch (_) {
+      emit(state.copyWith(status: FormzSubmissionStatus.failure));
+    }
   }
 }
