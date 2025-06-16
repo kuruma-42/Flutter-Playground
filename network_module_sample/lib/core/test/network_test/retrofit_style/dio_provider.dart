37567d0d16af77ec3af312b50b8fb61ce1895c07
YongJunCha <60722292+kuruma-42@users.noreply.github.com>
2025-06-16 16:20:37 +0900

    chore: sample 코드 삭제

--- network_module_sample/lib/core/test/network_test/retrofit_style/dio_provider.dart
+++
@@ -1,7 +0,0 @@
-import 'package:dio/dio.dart';
-
-Dio dioProvider(BaseOptions options) {
-  final dio = Dio(options);
-  dio.interceptors.add(LogInterceptor(requestBody: true, responseBody: true));
-  return dio;
-}
