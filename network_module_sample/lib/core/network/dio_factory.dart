466ecec64c72bf3916b5f2c147036b95be8c26df
YongJunCha <60722292+kuruma-42@users.noreply.github.com>
2025-06-16 16:34:22 +0900

    feat: dio_factory -> abstract interface class 로 변경

--- network_module_sample/lib/core/network/dio_factory.dart
+++ network_module_sample/lib/core/network/dio_factory.dart
@@ -1,7 +1,7 @@
 import 'package:dio/dio.dart';
 import 'package:injectable/injectable.dart';
 
-abstract class DioFactory {
+abstract interface class DioFactory {
   Dio create(String baseUrl);
 }
 
